6e6ee2a63f88128dac1d05b54f2e09f7143d6599
Takeshi.Hirosue <mirrored1976@gmail.com>
2021-03-08 06:06:04 +0900

    feat: add holiday view

--- lib/ui/holiday/holiday.dart
+++ lib/ui/holiday/holiday.dart
@@ -1,19 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
-class Holiday extends StatelessWidget {
-  static const String _title = 'Holiday';
+class Holiday extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _HolidayApp();
+}
 
+class _HolidayApp extends State<Holiday> {
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Container(
-        child: const Text(
-          _title,
-          style: TextStyle(
-            fontSize: 25,
+      child: Scaffold(
+        body: Container(
+          child: SfCalendar(
+            view: CalendarView.month,
+            todayHighlightColor: Colors.red,
+            showNavigationArrow: true,
           ),
         ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            //何かする
+            print('add click');
+          },
+          tooltip: 'Increment',
+          child: const Icon(Icons.add),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
       ),
     );
   }
