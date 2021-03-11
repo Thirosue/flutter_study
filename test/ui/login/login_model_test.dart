1d57ffe050af680e3aa2c5106952c3cf22a419ba
Takeshi.Hirosue <mirrored1976@gmail.com>
2021-03-12 06:44:07 +0900

    feat: add login model test case

--- test/ui/login/login_model_test.dart
+++ test/ui/login/login_model_test.dart
@@ -53,7 +53,11 @@ class MockAuthRepository implements AuthRepository {
 }
 
 void main() {
-  group('LoginModel auth() ', () {
+  final target = LoginModel(
+    MockAuthRepository(),
+  );
+
+  group('1. LoginModel auth() ', () {
     test('1-1. 認証APIが正常終了するとき、セッションの中身が正常に検証できること', () async {
       var mock = MockAuthRepository();
 
@@ -61,11 +65,55 @@ void main() {
       //   return Future.value(response);
       // });
 
-      var target = LoginModel(mock);
-      var session = await target.auth();
+      var model = LoginModel(mock);
+      var session = await model.auth();
 
       expect(session.idToken,
           "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c");
+    });
+  });
+
+  group('2. LoginModel emptyValidator ', () {
+    test('2-1. value が null のとき、エラー文言を返すこと', () {
+      var message = target.emptyValidator(null);
+
+      print(message);
+      expect(message?.isNotEmpty, true);
+    });
+
+    test('2-2. value が 空文字 のとき、エラー文言を返すこと', () {
+      var message = target.emptyValidator('');
+
+      print(message);
+      expect(message?.isNotEmpty, true);
+    });
+
+    test('2-3. value が 半角スペース のとき、nullを返すこと', () {
+      var message = target.emptyValidator(' ');
+
+      expect(message, null);
+    });
+
+    test('2-4. value が 全角スペース のとき、nullを返すこと', () {
+      var message = target.emptyValidator('　');
+
+      expect(message, null);
+    });
+
+    test('2-5. value が 文字列「password」 のとき、nullを返すこと', () {
+      var message = target.emptyValidator('password');
+
+      expect(message, null);
+    });
+  });
+
+  group('3. LoginModel togglePasswordVisible() ', () {
+    test('1-1. toggleをコールするごとに、showPasswordが切り替わること', () {
+      expect(target.showPassword, false);
+      target.togglePasswordVisible();
+      expect(target.showPassword, true);
+      target.togglePasswordVisible();
+      expect(target.showPassword, false);
     });
   });
 }
