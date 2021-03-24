ee0a5c473bfb335211395acaadb2c9add026ac5a
Takeshi.Hirosue <mirrored1976@gmail.com>
2021-03-25 07:01:03 +0900

    wip: implements booking form

--- lib/ui/booking/register/booking_model.dart
+++ lib/ui/booking/register/booking_model.dart
@@ -4,7 +4,19 @@ import '../../../repository/booking_repository.dart';
 
 class BookingModel extends ChangeNotifier {
   final BookingRepository repository;
+  String day = '';
+  String time = '';
+  int cols = 2;
+  String name = '';
+  String tel = '';
   String request = '';
 
   BookingModel(this.repository);
+
+  Future<void> save() async {
+    print("time: $day $time, cols: $cols,"
+        " name: $name, tel: $tel, request: $request");
+
+    await repository.create();
+  }
 }
--- lib/ui/booking/register/booking_page.dart
+++ lib/ui/booking/register/booking_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/constants.dart';
+import 'package:gap/gap.dart';
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 
+import '../../../constants.dart';
 import '../../../repository/booking_repository.dart';
 import 'booking_model.dart';
 
@@ -26,27 +27,77 @@ class BookingApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final model = context.read<BookingModel>();
+
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(Constants.calendarTitle),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: <Widget>[
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      labelText: 'Request',
+      appBar: AppBar(
+        title: const Text(Constants.calendarTitle),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Container(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: '予約日',
+                  ),
+                  onSaved: (value) => model.day = value!,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: '時刻',
+                  ),
+                  onSaved: (value) => model.time = value!,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: '時間枠',
+                  ),
+                  onSaved: (value) => model.cols,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: '名前',
+                  ),
+                  onSaved: (value) => model.name = value!,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: '電話番号',
+                  ),
+                  onSaved: (value) => model.tel = value!,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'メモ',
+                  ),
+                  onSaved: (value) => model.request = value!,
+                ),
+                const Gap(8),
+                Container(
+                  width: double.infinity,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          _formKey.currentState!.save();
+
+                          await model.save();
+                        }
+                      },
+                      child: const Text('予約する'),
                     ),
-                    onSaved: (value) => context.read<BookingModel>().request,
-                  )
-                ],
-              ),
+                  ),
+                )
+              ],
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
