59f04a3a03a119c0a90c1fe948c023da133fda47
Takeshi.Hirosue <mirrored1976@gmail.com>
2021-03-28 06:43:10 +0900

    fix: test failed

--- test/repository/booking_repository_test.dart
+++ test/repository/booking_repository_test.dart
@@ -1,12 +1,25 @@
+import 'package:flutter_app/model/calendar/booking.dart';
 import 'package:flutter_app/repository/booking_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:intl/intl.dart';
 
 void main() async {
   final repository = BookingRepository();
+  final booking = Booking(
+    day: DateFormat('yyyyMMdd').format(DateTime.now()),
+    time: DateTime.now().microsecondsSinceEpoch,
+    at: '1000',
+    cols: 4,
+    dateText: DateFormat('yyyy-MM-dd HH:mm').format(DateTime.now()),
+    name: 'john doe',
+    tel: 'dummy',
+    request: 'dummy',
+  );
 
   group('BookingRepository create() ', () {
     test('予約が正常に作成できること', () async {
-      await repository.create();
+      print(booking.toString());
+      await repository.create(booking);
       assert(true);
     });
   });
